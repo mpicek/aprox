0813e6927101c3c3ba5ca89389b5685e4b06542b
mpicek <martinpicek4@gmail.com>
2021-03-20 09:36:41 +0100

    Add basic distribution interface

--- main.cpp
+++ main.cpp
@@ -1,14 +1,96 @@
 #include <iostream>
 #include <unistd.h>
 #include <sstream>
+#include <map>
+#include <cmath>
+
+#define NUM_OF_RESULT_BINS_DEFAULT 25
 
 // Returns true on success, false on failure.
 // TODO: pridat const
-// Todo: getopt na parsovani argumentu
+// TODO: nastavitelny variance
 
 class Expression{
 
 };
+
+template <typename real> class Distribution{
+    std::map<real, real> distribution;
+    real bin_size;
+    real from;
+    real to;
+    bool wrong_distribution;
+    unsigned int num_of_bins;
+
+public:
+
+    Distribution(char type, real from_param, real to_param) {
+
+        from = round(from_param / bin_size);
+        to = round(to_param/bin_size);
+
+        // when from > to, the distribution is not correct
+        if(from > to){
+            wrong_distribution = true;
+            return;
+        }
+        else wrong_distribution = false;
+
+        num_of_bins = (to - from + bin_size) / bin_size;
+
+        switch (type){
+            case '~': // normal distribution
+                create_normal_distribution();
+                break;
+            case 'n': // also normal distribution
+                create_normal_distribution();
+                break;
+            
+            case 'u': // uniform distribution
+                create_uniform_distribution();
+                break;
+
+            case 'b': // binomial distribution
+                create_binomial_distribution();
+                break;
+        default:
+            break;
+        }
+
+
+    }
+
+    void create_normal_distribution(){
+
+    }
+
+    void create_uniform_distribution(){
+        real uniform_value = 1 / num_of_bins;
+
+        for(real i = from; i <= to; i += bin_size){
+            distribution[i] = uniform_value;
+        }
+    }
+
+    void create_binomial_distribution(){
+        
+    }
+
+
+    Distribution operator+(const Distribution &second){}
+    Distribution operator+(const int scalar){}
+
+    Distribution operator-(const Distribution &second){}
+    Distribution operator-(const int scalar){}
+
+    Distribution operator*(const Distribution &second){}
+    Distribution operator*(const int scalar){}
+
+    Distribution operator/(const Distribution &second){}
+    Distribution operator/(const int scalar){}
+
+};
+
 
 // real is the type that represents the real number
 template <typename real> class Program{
@@ -23,6 +105,7 @@ template <typename real> class Program{
     char* output_file_name;
 
     bool help_flag;
+    int num_of_result_bins;
 
     real bin_size; // size of the bins in which the distributions are stored
 
@@ -43,7 +126,8 @@ template <typename real> class Program{
 public:
     Program(int argc, char **argv) : argc(argc),
                                      argv(argv),
-                                     bin_size(1) {}
+                                     bin_size(1),
+                                     num_of_result_bins(NUM_OF_RESULT_BINS_DEFAULT) {}
 
 
     /**
@@ -53,11 +137,13 @@ public:
     bool parse_arguments(){
         int c;
         char* bin_size_char = nullptr;
+        char* result_bins_char = nullptr;
 
         // after argument : = it needs another argument
         // after argument :: = another argument is optional
         while((c = getopt(argc, argv, "i:o:b:h")) != -1){
             bool s_in_switch = false;
+            bool r_in_switch = false;
             switch (c){
                 case 'i': // input will be loaded from a file
                     input_flag = true;
@@ -71,6 +157,9 @@ public:
                     bin_size_char = optarg; // size of the bin has to follow
                     s_in_switch = true;
                     break;
+                case 'r': // how many bins to use during result presentation
+                    result_bins_char = optarg;
+                    r_in_switch = true;
                 case 'h': // print help
                     help_flag = true;
                     // don't read other options, just print help and quit
@@ -86,8 +175,17 @@ public:
             if(s_in_switch){
                 std::stringstream tmp(bin_size_char);
                 if(!(tmp >> bin_size)){
-                    std::cout << "ERROR: UNABLE TO READ BIN_SIZE, SETTING BIN_SIZE TO 1 (DEFAULT)." << std::endl;
+                    std::cout << "ERROR: UNABLE TO READ BIN SIZE, SETTING BIN_SIZE TO 1 (DEFAULT)." << std::endl;
                     bin_size = 1;
+                }
+            }
+
+            // In switch you are unable to create new variables TODO why?? ... this is the solution
+            if(r_in_switch){
+                std::stringstream tmp(result_bins_char);
+                if(!(tmp >> num_of_result_bins)){
+                    std::cout << "ERROR: UNABLE TO READ NUMBER OF BINS IN RESULT, SETTING NUM_OF_RUSULT_BINS TO 100 (DEFAULT)." << std::endl;
+                    num_of_result_bins = 1;
                 }
             }
         }
@@ -117,9 +215,10 @@ public:
         //Todo
         if(help_flag){
             std::cout << "Usage: aprox [-i in] [-o out] [-s interval size]" << std::endl;
-            std::cout << "    -i: input file (default: stdin)" << std::endl;
-            std::cout << "    -o: output file (default: stdout)" << std::endl;
+            std::cout << "    -i: input file, default: stdin" << std::endl;
+            std::cout << "    -o: output file, default: stdout" << std::endl;
             std::cout << "    -b: bin_size - size of the bins in which the distributions are stored, default = 1" << std::endl;
+            std::cout << "    -r: how many bins to use during result presentation, default = " << NUM_OF_RESULT_BINS_DEFAULT << std::endl;
             return true;
         }
         return false;
